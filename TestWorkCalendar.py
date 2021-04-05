d0452eb098bf6ace8473170700a8c17a7ecf190f
Brian Whiteley <brianwhiteley@Brians-Mac-mini.local>
2021-04-05 15:20:25 -0500

    add month

--- TestWorkCalendar.py
+++ TestWorkCalendar.py
@@ -3,12 +3,17 @@ import unittest
 from datetime import datetime
 
 class TestWorkCalendar(unittest.TestCase):
-    def test_init(self):
-        wc = WorkCalendar(2019)
-        self.assertEqual(2019, wc.year )
+    def test_init_without_arguments(self):
         cc = WorkCalendar()
         current_year = datetime.now().year
+        current_month = datetime.now().month
         self.assertEqual(cc.year, current_year)
+        self.assertEqual(cc.month, current_month)
+        
+    def test_init_with_arguments(self):
+        wc = WorkCalendar(year=2019, month=3)
+        self.assertEqual(2019, wc.year )
+        self.assertEqual(3, wc.month)
 
 
 if __name__ == '__main__':
--- workCalendar.py
+++ workCalendar.py
@@ -2,9 +2,14 @@ import calendar
 import datetime
 
 class WorkCalendar:
-    def __init__(self, year=datetime.datetime.now().year):
+    def __init__(self, 
+    year=datetime.datetime.now().year,
+    month=datetime.datetime.now().month):
         self.year = year
+        self.month = month
+
         
- 
+c = WorkCalendar(month=6)
+print(c.month)
 
 
